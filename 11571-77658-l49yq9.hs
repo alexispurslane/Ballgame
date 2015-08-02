aee2ff5ce876f1a657ac55487ac4ae24692aaf97
christopherdumas <christopherdumas@me.com>
2015-08-01 20:22:48 -0700

    added readme

---
+++ 11571-77658-l49yq9.hs
@@ -0,0 +1,8 @@
+Ballgame Scheme
+===
+A simplefied, clean, refreshing and new Scheme, with *first-class* macros
+---
+Ballgame Scheme aims to be mostly compatable with R6RS Scheme, while radically simplifying and improving many things that are seen as downsides, compared to most languages. Our goals are:
+
+* Simpler, more usable, and more powerful macros
+* First-class macros, becouse we can!
